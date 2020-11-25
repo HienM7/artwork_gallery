e72b77ce4ff8738e065ab494060a4f3a21199332
khoaDLuu <dangkhoa220899@gmail.com>
2020-11-25 08:23:47 +0700

    Create Artwork list view with no reference to users yet

---
+++ db/migrate/20201124103144_create_artworks.rb
@@ -0,0 +1,12 @@
+class CreateArtworks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :artworks do |t|
+      t.string :name
+      t.string :img_link
+      t.integer :value
+      t.integer :is_public
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+test_dir = 'TEST_artworks'
+artw_files = Dir.glob("app/assets/images/#{test_dir}/*") * 10
+
+default_artworks = artw_files.shuffle.collect { |af|
+  [
+    Faker::Movie.title,
+    "#{test_dir}/#{File.basename(af)}",
+    Faker::Number.within(range: 10..1000),
+    Faker::Boolean.boolean(true_ratio: 0.8) ? 1 : 0
+  ]
+}
+
+default_artworks.each do |name, path, value, is_public|
+  Artwork.create(
+    name: name,
+    img_link: path,
+    value: value,
+    is_public: is_public
+  )
+end
