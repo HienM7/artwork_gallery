4b08f96a4dc00d2acaca64c0f7d9ce70e47fee7a
Keith Dluu <dangkhoa240899@gmail.com>
2020-12-12 00:31:56 +0700

    Fix bug and improve UX for category management

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.order(created_at: :desc)
+    @categories = Category.order(updated_at: :desc)
   end
 
   # GET /categories/1
@@ -58,10 +58,17 @@ class CategoriesController < ApplicationController
   # DELETE /categories/1
   # DELETE /categories/1.json
   def destroy
-    @category.destroy
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully deleted.' }
-      format.json { head :no_content }
+    if true # mock for: @category.artworks.length > 0
+      respond_to do |format|
+        format.html { redirect_to categories_url, alert: 'Category was not deleted because it has artworks.' }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    else
+      @category.destroy
+      respond_to do |format|
+        format.html { redirect_to categories_url, notice: 'Category was successfully deleted.' }
+        format.json { head :no_content }
+      end
     end
   end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
 end
