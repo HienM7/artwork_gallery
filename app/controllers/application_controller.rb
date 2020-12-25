b8e3b144b8a06e0a761373e0bb0750381fc04a81
Keith Dluu <dangkhoa240899@gmail.com>
2020-12-25 10:24:12 +0700

    Update layout with partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+	layout :layout_by_resource
+	
 	before_action :update_allowed_parameters, if: :devise_controller?
 
 	protected
@@ -6,5 +8,17 @@ class ApplicationController < ActionController::Base
 	def update_allowed_parameters
 		devise_parameter_sanitizer.permit(:sign_up){|u| u.permit(:username, :password, :email, :credit_card, :point)}
 		devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:username, :email, :credit_card, :password, :current_password)}
+	end
+
+	def layout_by_resource
+		log_act = [
+			[:user, 'new'],
+			[:user, 'cancel']
+		]
+		if devise_controller? && log_act.include?([resource_name, action_name])
+			"auth"
+		else
+			"application"
+		end
 	end
 end
