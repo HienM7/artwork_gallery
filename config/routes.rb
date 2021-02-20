c714b09289941a70f513df99d2e08511c2551f42
Keith Dluu <dangkhoa240899@gmail.com>
2021-02-20 11:11:09 +0700

    Finish basic CRUD functionalities for Tagging

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
-  resources :tags
+  root to: 'artworks#index', as: 'home'
+
   devise_for :users, controllers: { registrations: 'registrations' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # namespace :admin do
-  #   root to: "admin#index"
-  # end
+
   scope '/admin' do
+    # resources :categories, only: [:index]
     resources :categories
+    resources :tags
     resources :users
   end
 
   resources :artworks
-
-  # resources :categories, only: [:index]
-
-  root to: 'artworks#index', as: 'home'
 
   post 'artworks/:id/download', to: 'artworks#download', as: 'download'
   get 'my/artworks', to: 'artworks#my_artworks', as: 'show_my_artworks'
